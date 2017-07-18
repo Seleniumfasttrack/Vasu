5c6a411a774c183b96c0c093fad95d4be365e95b
Rakesh <rakesh.nelluri.kumar@gmail.com>
2017-07-18 15:50:42 +0530

    tt test demo

---
+++ nelluri/src/main/java/rakesh/nelluri/App.java
@@ -0,0 +1,13 @@
+package rakesh.nelluri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ nelluri/src/main/java/rakesh/nelluri/testgit.java
@@ -0,0 +1,17 @@
+package rakesh.nelluri;
+
+public class testgit {
+
+	public static void main(String[] args) {
+
+	int a=10;
+	if (a>18) {
+		System.out.println("adult");
+		
+	}
+	else {
+		System.out.println("minor");
+	}
+	}
+
+}
